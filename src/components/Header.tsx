3433d65258d9d111ab78bffa71f1010ab877f14e
Mykyta <161162416+ZozuliaMykyta@users.noreply.github.com>
2025-10-14 17:04:14 +0200

    feat: add Header component with styled title

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header: React.FC = () => {
+  return (
+    <h1 className="header-container font-family-sigmar text-[#7F6F3E] text-[38px]">
+      PortFolio
+    </h1>
+  );
+};
+
+export default Header;
