93c61e93aa23736094d42b6f5ff72e308552c2be
Mykyta <161162416+ZozuliaMykyta@users.noreply.github.com>
2025-10-14 17:09:48 +0200

    style: adjust padding in body and update Header font size for responsiveness

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header: React.FC = () => {
   return (
-    <h1 className="header-container font-family-sigmar text-[#7F6F3E] text-[38px]">
+    <h1 className="header-container font-family-sigmar text-[#7F6F3E] text-[20px] sm:text-[24px] md:text-[28px] lg:text-[32px] xl:text-[38px] text-center sm:text-left">
       PortFolio
     </h1>
   );
