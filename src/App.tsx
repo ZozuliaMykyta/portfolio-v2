417a17d446e74ddce956f33bbb9e16ed258b183c
Mykyta <161162416+ZozuliaMykyta@users.noreply.github.com>
2025-10-16 15:45:18 +0200

    feat: include AboutMe component in App layout for enhanced content display

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import AboutMe from "./components/About/AboutMe";
 import Header from "./components/Header";
 import Info from "./components/Info";
 
@@ -8,7 +9,9 @@ function App() {
       <Header />
       <div className="container flex justify-between items-center !mt-15">
         <Info />
-        <main></main>
+        <main>
+          <AboutMe />
+        </main>
       </div>
     </>
   );
