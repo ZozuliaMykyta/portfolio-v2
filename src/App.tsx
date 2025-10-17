b9a8cb8e2cbf4416c9e89566e48fbeec3bb0d52d
Mykyta <161162416+ZozuliaMykyta@users.noreply.github.com>
2025-10-17 11:07:17 +0200

    feat: implement section-based rendering in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import "./App.css";
 import AboutMe from "./components/About/AboutMe";
+import { useSectionContext } from "./components/contexts/SectionContext";
 import Header from "./components/Header";
 import Info from "./components/Info";
 import SectionNav from "./components/Navigation/SectionNav";
 
 function App() {
+  const { currentSection } = useSectionContext();
   return (
     <>
       <Header />
       <div className="container flex justify-between items-center !mt-15 gap-8">
         <Info />
         <main>
-          <AboutMe />
+          {currentSection === "about" && <AboutMe />}
+          {currentSection === "experience" && <div>Experience Section</div>}
         </main>
         <SectionNav />
       </div>
