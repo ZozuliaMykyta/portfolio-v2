619c17519b2afdb9720d59502f4e38e3eb88a20a
Mykyta <161162416+ZozuliaMykyta@users.noreply.github.com>
2025-10-17 11:07:06 +0200

    feat: wrap App component with SectionProvider for section state management

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import { SectionProvider } from "./components/contexts/SectionProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <SectionProvider>
+      <App />
+    </SectionProvider>
   </StrictMode>
 );
