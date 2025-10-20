9313310f351d747e90c542723a502f4849a2b2bc
Mykyta <161162416+ZozuliaMykyta@users.noreply.github.com>
2025-10-20 21:51:48 +0200

    fix: enhance layout and styling of Experience component for improved responsiveness

--- src/components/Experience/experience.tsx
+++ src/components/Experience/experience.tsx
@@ -2,9 +2,11 @@ import React from "react";
 
 const experience: React.FC = () => {
   return (
-    <div className="dark-bg pt-5 pb-8 px-9">
-      <h2 className="text-[#FDFAF0] font-bold text-[28px]">Experience</h2>
-      <p className="mt-5 text-[#FDFAF0]">
+    <div className="dark-bg pt-5 pb-8 px-4 sm:px-6 md:px-8 2xl:px-8">
+      <h2 className="text-[#FDFAF0] font-bold text-[28px] sm:text-[30px] md:text-[32px] 2xl:text-[28px]">
+        Experience
+      </h2>
+      <p className="mt-5 text-[#FDFAF0] text-[16px] sm:text-[20px] md:text-[22px] 2xl:text-[18px] leading-relaxed">
         I completed my first freelance project end I completed my first
         freelance project end to end—from clarifying requirements through
         development, testing, and handover. I also take on student mini-projects
