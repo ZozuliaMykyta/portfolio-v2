7d52f0e61810db9243a09a99334eb15ecc11b2c0
Mykyta <161162416+ZozuliaMykyta@users.noreply.github.com>
2025-10-20 12:55:29 +0200

    feat: add background property to Next.js skill and update styling for skills display

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -15,7 +15,7 @@ const Skills: React.FC = () => {
     { name: "TypeScript", icon: typeScript },
     { name: "JavaScript", icon: javaScript },
     { name: "React", icon: reactIcon },
-    { name: "Next.js", icon: nextjs },
+    { name: "Next.js", icon: nextjs, bg: true },
     { name: "Node.js", icon: nodejs },
     { name: "Express", icon: express },
     { name: "MongoDB", icon: mongodb },
@@ -31,7 +31,7 @@ const Skills: React.FC = () => {
           <div className="flex flex-col justify-center items-center">
             <div
               key={skill.name}
-              className="border-[1px] border-solid border-custom-brown rounded-[8px] p-2.5"
+              className={`border-[1px] border-solid border-custom-brown rounded-[8px] p-2.5 bg-gray-300`}
               style={{ boxShadow: "0 0 5.5px 1px #EFE0B2" }}
             >
               <img src={skill.icon} alt={skill.name} className="w-[60px]" />
