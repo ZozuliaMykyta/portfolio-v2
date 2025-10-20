f01b4db355d1be97d0459acca1fb3dd0b475bdea
Mykyta <161162416+ZozuliaMykyta@users.noreply.github.com>
2025-10-20 19:54:31 +0200

    fix: enhance styling and structure of ContactMe component for better user experience

--- src/components/contact/ContactMe.tsx
+++ src/components/contact/ContactMe.tsx
@@ -2,22 +2,35 @@ import React from "react";
 
 const ContactMe: React.FC = () => {
   return (
-    <div>
-      <h2>Contact Me</h2>
-      <form action="#!">
+    <div className="dark-bg pt-5 pb-8 px-9">
+      <h2 className="text-[28px] font-bold text-white">Contact Me</h2>
+      <form
+        action="#!"
+        className="flex flex-col justify-center items-center gap-8 mt-11"
+      >
         <div>
-          <h6>Email Address</h6>
-          <input type="email" name="email" />
+          <h6 className="text-[20px] text-white font-medium mb-1.5">
+            Email Address
+          </h6>
+          <input type="email" name="email" className="contactInput" />
         </div>
         <div>
-          <h6>Subject</h6>
-          <input type="text" name="subject" />
+          <h6 className="text-[20px] text-white font-medium mb-1.5">Subject</h6>
+          <input type="text" name="subject" className="contactInput" />
         </div>
         <div>
-          <h6>Message</h6>
-          <textarea name="message"></textarea>
+          <h6 className="text-[20px] text-white font-medium mb-1.5">Message</h6>
+          <textarea
+            name="message"
+            className="contactInput resize-none"
+          ></textarea>
         </div>
-        <button type="submit">Submit</button>
+        <button
+          type="submit"
+          className="text-[21px] font-family-merriweather text-black font-bold bg-custom-brown border-2 border-solid border-custom-brown rounded-[8px] py-3 px-14"
+        >
+          Submit
+        </button>
       </form>
     </div>
   );
