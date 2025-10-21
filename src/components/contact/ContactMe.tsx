d81cb21a660ecf718a655fa58b2fad02beab197a
Mykyta <161162416+ZozuliaMykyta@users.noreply.github.com>
2025-10-21 09:25:17 +0200

    fix: enhance layout and styling of ContactMe component for improved consistency and responsiveness

--- src/components/contact/ContactMe.tsx
+++ src/components/contact/ContactMe.tsx
@@ -3,31 +3,48 @@ import React from "react";
 const ContactMe: React.FC = () => {
   return (
     <div className="dark-bg main-content">
-      <h2 className="text-[28px] font-bold text-white">Contact Me</h2>
+      <h2 className="text-[28px] 2xl:text-[28px] text-center 2xl:text-left font-bold text-white">
+        Contact Me
+      </h2>
       <form
         action="#!"
-        className="flex flex-col justify-center items-center gap-8 mt-11"
+        className="flex flex-col justify-center items-center gap-5 mt-7 w-full min-w-0 sm:min-w-[350px] max-w-[600px] 2xl:min-w-[700px]"
       >
-        <div>
-          <h6 className="text-[20px] text-white font-medium mb-1.5">
+        <div className="w-full">
+          <h6 className="text-[18px] 2xl:text-[20px] text-white font-medium mb-1.5">
             Email Address
           </h6>
-          <input type="email" name="email" className="contactInput" />
-        </div>
-        <div>
-          <h6 className="text-[20px] text-white font-medium mb-1.5">Subject</h6>
-          <input type="text" name="subject" className="contactInput" />
+          <input
+            type="email"
+            name="email"
+            className="contactInput text-white w-full"
+            autoComplete="email"
+          />
         </div>
         <div className="w-full">
-          <h6 className="text-[20px] text-white font-medium mb-1.5">Message</h6>
+          <h6 className="text-[18px] 2xl:text-[20px] text-white font-medium mb-1.5">
+            Subject
+          </h6>
+          <input
+            type="text"
+            name="subject"
+            className="contactInput text-white w-full"
+            autoComplete="off"
+          />
+        </div>
+        <div className="w-full">
+          <h6 className="text-[18px] 2xl:text-[20px] text-white font-medium mb-1.5">
+            Message
+          </h6>
           <textarea
             name="message"
-            className="contactInput resize-none w-full"
+            className="contactInput resize-none w-full text-white h-[150px]"
+            autoComplete="off"
           ></textarea>
         </div>
         <button
           type="submit"
-          className="text-[21px] font-family-merriweather text-black font-bold bg-custom-brown border-2 border-solid border-custom-brown rounded-[8px] py-3 px-14"
+          className="cursor-pointer text-[18px] 2xl:text-[21px] font-family-merriweather text-black font-bold bg-custom-brown border-2 border-solid border-custom-brown rounded-[8px] py-3 px-8 2xl:px-14 w-full 2xl:w-auto transition-all duration-300 ease-in-out hover:bg-yellow-600 hover:border-yellow-600 hover:scale-105 hover:shadow-lg active:scale-95 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-opacity-50"
         >
           Submit
         </button>
